fa32a7dbd9f696d5d0ec32890af6ecb5d93a6756
Dariana Roman <alisadariana@gmail.com>
2021-10-19 18:12:43 +0300

    lab3

---
+++ Students/Roman Alisa-Dariana/lab3/src/com/studpack/classes/Student.java
@@ -0,0 +1,13 @@
+package com.studpack.classes;
+
+public class Student {
+    public String uniqueId;
+    public String firstName;
+    public String lastName;
+
+    public Student(String uniqueID, String firstName, String lastName) {
+        this.uniqueId = uniqueID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
