f4fe5ca4504903306c0bb1466e49fe8b8dc60076
Veres Noémi <veres.noemi@yahoo.com>
2021-10-25 09:48:45 +0300

    This is my homework from lab 4

---
+++ Students/Veres Noemi/LAB4/src/ro/my/Shop/Category.java
@@ -0,0 +1,11 @@
+package ro.my.Shop;
+
+public class Category {
+    public String name;
+    public int ID;
+
+    public Category(String name, int ID){
+        this.name = name;
+        this.ID = ID;
+    }
+}
