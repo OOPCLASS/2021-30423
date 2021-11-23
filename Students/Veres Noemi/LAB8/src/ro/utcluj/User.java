9a0637391a5eb736cb28eb5f97fe0d45f66ce164
Veres Noémi <veres.noemi@yahoo.com>
2021-11-23 10:48:02 +0200

    Finished assignment from Lab 8

---
+++ Students/Veres Noemi/LAB8/src/ro/utcluj/User.java
@@ -0,0 +1,44 @@
+package ro.utcluj;
+
+public class User {
+    private String name;
+    private int id;
+
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        User user = (User) obj; // type casting
+        return (user.name.equals(this.name) && user.id == this.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+}
